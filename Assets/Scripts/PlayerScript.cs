c3a2bbcfa7dc38a2f426be2a09f989f96e3ca303
rchtr-chn <richter.cheniago.2006@gmail.com>
2025-02-17 23:51:24 +0700

    Programming minigame update
    bug: pencet ENTER setelah "koding" gabisa kelar promptnya

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    // WORDBANK----
+
+
+    public Text wordOutput;
+    public Text prompt;
+    private string remainingWord = string.Empty;
+    private string currentString = "test";
+    private string inputString = string.Empty;
+
+    private void Start()
+    {
+        //TEST, REMOVE AFTER ADDING WORDBANK
+        //--------
+
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord()
+    {
+        //GET STRING FROM WORDBANK
+        SetRemainingWord(currentString);
+    }
+
+    private void SetRemainingWord(string newString)
+    {
+        remainingWord = newString;
+        prompt.text = currentString;
+        wordOutput.text = inputString;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if(Input.anyKeyDown)
+        {
+            if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                Debug.Log("ENTER was pressed");
+                return;
+            }
+
+            string keyPressed = Input.inputString;
+
+            if(keyPressed.Length == 1 )
+            {
+                EnterLetter(keyPressed);
+            }
+        }
+    }
+
+    private void EnterLetter(string typedLetter)
+    {
+        if(IsCorrectLetter(typedLetter))
+        {
+            AddLetter(typedLetter);
+            RemoveLetter();
+
+            if(IsComplete() && Input.GetKeyDown(KeyCode.Return))
+            {
+                inputString = string.Empty;
+                SetCurrentWord();
+            }
+        }
+    }
+
+    private bool IsCorrectLetter(string letter)
+    {
+        return remainingWord.IndexOf(letter) == 0;
+    }
+
+    private void AddLetter(string typedLetter)
+    {
+        inputString = inputString + typedLetter;
+    }
+
+    private void RemoveLetter()
+    {
+        string newString = remainingWord.Remove(0, 1);
+        SetRemainingWord(newString);
+    }
+
+    private bool IsComplete()
+    {
+        return remainingWord.Length == 0;
+    }
+}
