c33d87d4e17683f6dfef8ed339f8a4167fad7f48
rchtr-chn <richter.cheniago.2006@gmail.com>
2025-02-21 17:09:53 +0700

    added wave script & cellphone minigame

--- Assets/Scripts/ChatBubbleSpawnerScript.cs
+++ Assets/Scripts/ChatBubbleSpawnerScript.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class ChatBubbleSpawnerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject prefab;
+    public List<GameObject> toSpawn = new List<GameObject>();
+    public WaveScript waveScript;
+    int mockWave = 0;
+    public int availablePos = 4;
+
+    public void Awake()
     {
-        
+        waveScript = GameObject.Find("Player").GetComponent<WaveScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(mockWave < waveScript.currentWave && waveScript.currentWave > 6)
+        {
+            if (waveScript.currentWave == 7)
+            {
+                mockWave = waveScript.currentWave;
+                for (int i = 0; i < 2; i++)
+                {
+                    toSpawn.Add(prefab);
+                }
+            }
+            else if (waveScript.currentWave == 8)
+            {
+                mockWave = waveScript.currentWave;
+                for (int i = 0; i < 3; i++)
+                {
+                    toSpawn.Add(prefab);
+                }
+            }
+            else if (waveScript.currentWave == 9)
+            {
+                mockWave = waveScript.currentWave;
+                for (int i = 0; i < 5; i++)
+                {
+                    toSpawn.Add(prefab);
+                }
+            }
+            if (waveScript.currentWave >= 10)
+            {
+                mockWave = waveScript.currentWave;
+                for (int i = 0; i < 7 + 2 * (waveScript.currentWave - 10); i++)
+                {
+                    toSpawn.Add(prefab);
+                }
+            }
+        }
+
+        SpawnEnt();
     }
+
+    void SpawnEnt()
+    {
+        for(int i = 0 ; i < toSpawn.Count ; i++)
+        {
+            if (availablePos > 0 && toSpawn.Contains(prefab))
+            {
+                GameObject chatBubble = (GameObject)Instantiate(toSpawn[i]);
+                toSpawn.RemoveAt(0);
+            }
+        }
+    }
+
 }
--- Assets/Scripts/WaveScript.cs
+++ Assets/Scripts/WaveScript.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 
 public class WaveScript : MonoBehaviour
 {
-    float timer;
+    private float totalElapsedTime = 0f;
+    public int currentWave;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        //totalElapsedTime += Time.deltaTime;
+        //currentWave = GetCurrentWave(totalElapsedTime);
+        currentWave = 9;
+        Debug.Log($"Current Wave: {currentWave} | Elapsed Time: {totalElapsedTime:F2} sec");
+    }
+
+    int GetCurrentWave(float elapsedTime)
+    {
+        float timeTracker = 0f;
+
+        // Wave 1 (10 sec)
+        if (elapsedTime < 10f)
+            return 1;
+        timeTracker += 10f;
+
+        // Waves 2-4 (15 sec each)
+        for (int wave = 2; wave <= 4; wave++)
+        {
+            if (elapsedTime < timeTracker + 15f)
+                return wave;
+            timeTracker += 15f;
+        }
+
+        // Waves 5-7 (20 sec each)
+        for (int wave = 5; wave <= 7; wave++)
+        {
+            if (elapsedTime < timeTracker + 20f)
+                return wave;
+            timeTracker += 20f;
+        }
+
+        // Wave 8 (25 sec)
+        if (elapsedTime < timeTracker + 25f)
+            return 8;
+        timeTracker += 25f;
+
+        // Wave 9+ (incrementing by 5 sec per wave)
+        int waveNum = 9;
+        float nextWaveDuration = 30f;
+
+        while (elapsedTime >= timeTracker + nextWaveDuration)
+        {
+            timeTracker += nextWaveDuration;
+            nextWaveDuration += 5f;
+            waveNum++;
+        }
+
+        return waveNum;
     }
 }
