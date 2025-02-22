5acbb1f1145b3b64896b9ccecd1c0db293d0c8b2
ChocoMOCC <157827789+ChocoMOCC@users.noreply.github.com>
2025-02-22 23:26:40 +0700

    startscreen
    wjefb

--- Assets/Scripts/OptionsScript.cs
+++ Assets/Scripts/OptionsScript.cs
@@ -1,48 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsScript : MonoBehaviour
 {
-    public TMPro.TMP_Dropdown resDropdown;
-    Resolution[] resolutions;
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider musicSlider;
 
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-
-        resDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-        resDropdown.AddOptions(options);
-        resDropdown.value = currentResolutionIndex;
-        resDropdown.RefreshShownValue();
-    }
-
-    public void setFullscreen(bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
-    }
-    
     public void setmusicVolume(float volume)
     {
         audioMixer.SetFloat("volume", Mathf.Log10(volume)*10);
     }
 }
-
-
--- Assets/Scripts/StartScreenManager.cs
+++ Assets/Scripts/StartScreenManager.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 public class StartScreenManager : MonoBehaviour
 {
     public GameObject StartManager;
+    public GameObject OptionsMenu; 
+
     public Text pressSpace;
     public Transform Target;
+    public Transform OptionsTarget;
     public float zoomingSpeed;
 
     private bool isZooming = false;
     private Camera mainCamera;
     private float initialSize;
     private Vector3 initialPosition;
+    private float targetSize;
 
     void Start()
     {
@@ -21,6 +25,8 @@ public class StartScreenManager : MonoBehaviour
         initialSize = mainCamera.orthographicSize;
         initialPosition = mainCamera.transform.position;
         StartManager.SetActive(false);
+        OptionsMenu.SetActive(false);
+        targetSize = initialSize * 0.6f;
     }
 
     void Update()
@@ -28,36 +34,55 @@ public class StartScreenManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && !isZooming)
         {
             Debug.Log("Space Pressed");
-            StartCoroutine(ZoomIn());   
+            StartCoroutine(ZoomIn(Target, true));
         }
     }
 
-    IEnumerator ZoomIn()
+    IEnumerator ZoomIn(Transform newTarget, bool showStartMenu)
     {
         isZooming = true;
         pressSpace.enabled = false;
 
-        Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, mainCamera.transform.position.z - 5f);
-        float targetSize = initialSize * 0.6f;
+        Vector3 targetPosition = new Vector3(newTarget.position.x, newTarget.position.y, mainCamera.transform.position.z - 5f);
 
         float t = 0;
         while (t < 1)
         {
             t += Time.deltaTime * zoomingSpeed;
-            mainCamera.transform.position = Vector3.Lerp(initialPosition, targetPosition, t);
-            mainCamera.orthographicSize = Mathf.Lerp(initialSize, targetSize, t);
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, targetPosition, t);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, t);
             yield return null;
         }
 
-        StartManager.SetActive(true);
+        StartManager.SetActive(showStartMenu);
+        OptionsMenu.SetActive(!showStartMenu);
+
+        isZooming = false;
     }
 
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
+
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        if (!isZooming)
+        {
+            StartCoroutine(ZoomIn(OptionsTarget, false));
+        }
+    }
+
+    public void BackToMenu()
+    {
+        if (!isZooming)
+        {
+            StartCoroutine(ZoomIn(Target, true));
+        }
     }
 }
