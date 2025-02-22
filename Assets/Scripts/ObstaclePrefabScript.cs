7129e1f766e35d85a67e9f38fa1904c077b87448
rchtr-chn <richter.cheniago.2006@gmail.com>
2025-02-22 21:36:21 +0700

    fgn ajkgbjuaiekgnoiuaega

--- Assets/Scripts/ObstaclePrefabScript.cs
+++ Assets/Scripts/ObstaclePrefabScript.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class ObstaclePrefabScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed;
     private PlayerMovementScript playerMovementScript;
+    private ObstacleSpawnerScript obstacleSpawnerScript;
     private Transform spawnerPos;
     void Start()
     {
         playerMovementScript = GameObject.Find("Player").GetComponent<PlayerMovementScript>();
+        obstacleSpawnerScript = GameObject.Find("Obstacle-Spawner").GetComponent<ObstacleSpawnerScript>();
         spawnerPos = GameObject.Find("Obstacle-Spawner").GetComponent<Transform>();
     }
     void Update()
@@ -28,7 +29,7 @@ public class ObstaclePrefabScript : MonoBehaviour
         }
         else
         {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            rb.velocity = new Vector2(-obstacleSpawnerScript.prefabSpeed, rb.velocity.y);
         }
     }
 }
