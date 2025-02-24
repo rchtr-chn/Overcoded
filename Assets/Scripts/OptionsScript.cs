a937f1f6d26b08f6d3087eae9c88bee5bc3591c9
ChocoMOCC <157827789+ChocoMOCC@users.noreply.github.com>
2025-02-24 18:07:04 +0700

    harusnya si last ya

--- Assets/Scripts/OptionsScript.cs
+++ Assets/Scripts/OptionsScript.cs
@@ -14,12 +14,21 @@ public class OptionsScript : MonoBehaviour
         resDropdown.ClearOptions();
 
         List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width ==  Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resDropdown.AddOptions(options);
+        resDropdown.value = currentResolutionIndex;
+        resDropdown.RefreshShownValue();
     }
 
     public void setFullscreen(bool isFullscreen)
